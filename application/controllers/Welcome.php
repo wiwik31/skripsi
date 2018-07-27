3c9c3be32a6dc8fc25155fcddf291066c89892f3
wiwik ramna <wiwikramna@gmail.com>
2018-07-27 14:44:17 +0800

    1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,10 +1,16 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
+ 
 class Welcome extends CI_Controller {
-	
+	function __construct(){
+		parent ::__construct();
+	}
+
+	public function home(){
+		$this->load->view('templates/index');
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('templates/index');
 	}
 }
