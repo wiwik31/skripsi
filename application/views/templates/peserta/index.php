ae9367294d8f8a11afa1b82be626b41699043b30
akbarabustng <akbarabustang>
2018-10-08 16:59:50 +0700

    hapus index.php

--- application/views/templates/peserta/index.php
+++ application/views/templates/peserta/index.php
@@ -14,22 +14,22 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php base_url()?>template_for_peserta/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
 
     <!-- Waves Effect Css -->
-    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+    <link href="<?php base_url()?>template_for_peserta/plugins/node-waves/waves.css" rel="stylesheet" />
 
     <!-- Animation Css -->
-    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+    <link href="<?php base_url()?>template_for_peserta/plugins/animate-css/animate.css" rel="stylesheet" />
 
     <!-- Morris Chart Css-->
-    <link href="plugins/morrisjs/morris.css" rel="stylesheet" />
+    <link href="<?php base_url()?>template_for_peserta/plugins/morrisjs/morris.css" rel="stylesheet" />
 
     <!-- Custom Css -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="<?php base_url()?>template_for_peserta/css/style.css" rel="stylesheet">
 
     <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-    <link href="css/themes/all-themes.css" rel="stylesheet" />
+    <link href="<?php base_url()?>template_for_peserta/css/themes/all-themes.css" rel="stylesheet" />
 </head>
 
 <body class="theme-red">
@@ -1105,46 +1105,46 @@
     </section>
 
     <!-- Jquery Core Js -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core Js -->
-    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/bootstrap/js/bootstrap.js"></script>
 
     <!-- Select Plugin Js -->
-    <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/bootstrap-select/js/bootstrap-select.js"></script>
 
     <!-- Slimscroll Plugin Js -->
-    <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
 
     <!-- Waves Effect Plugin Js -->
-    <script src="plugins/node-waves/waves.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/node-waves/waves.js"></script>
 
     <!-- Jquery CountTo Plugin Js -->
-    <script src="plugins/jquery-countto/jquery.countTo.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/jquery-countto/jquery.countTo.js"></script>
 
     <!-- Morris Plugin Js -->
-    <script src="plugins/raphael/raphael.min.js"></script>
-    <script src="plugins/morrisjs/morris.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/raphael/raphael.min.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/morrisjs/morris.js"></script>
 
     <!-- ChartJs -->
-    <script src="plugins/chartjs/Chart.bundle.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/chartjs/Chart.bundle.js"></script>
 
     <!-- Flot Charts Plugin Js -->
-    <script src="plugins/flot-charts/jquery.flot.js"></script>
-    <script src="plugins/flot-charts/jquery.flot.resize.js"></script>
-    <script src="plugins/flot-charts/jquery.flot.pie.js"></script>
-    <script src="plugins/flot-charts/jquery.flot.categories.js"></script>
-    <script src="plugins/flot-charts/jquery.flot.time.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/flot-charts/jquery.flot.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/flot-charts/jquery.flot.resize.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/flot-charts/jquery.flot.pie.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/flot-charts/jquery.flot.categories.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/flot-charts/jquery.flot.time.js"></script>
 
     <!-- Sparkline Chart Plugin Js -->
-    <script src="plugins/jquery-sparkline/jquery.sparkline.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/plugins/jquery-sparkline/jquery.sparkline.js"></script>
 
     <!-- Custom Js -->
-    <script src="js/admin.js"></script>
-    <script src="js/pages/index.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/js/admin.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/js/pages/index.js"></script>
 
     <!-- Demo Js -->
-    <script src="js/demo.js"></script>
+    <script src="<?php base_url()?>template_for_peserta/js/demo.js"></script>
 </body>
 
 </html>
